9a5960f10c9d30e0f31bb4c266c8351e9f2fec40
Jasper Lee <jasperlee@Jaspers-MacBook-Pro-4.local>
2024-04-28 18:16:51 -0500

    changed eval frequency to 500 iterations and eval runs to 30 runs; getting ready to re-run everything

--- distill.py
+++ distill.py
@@ -132,11 +132,11 @@ def distill(teacher_model, student_model, env, student_led, n_iter, criterion, o
 
         if i % 100 == 0:
             print(i)
-        if i % 1000 == 0:
-            rewards, ep_lengths = evaluate_policy(student_model, eval_env, return_episode_rewards=True)
+        if i % 500 == 0:
+            rewards, ep_lengths = evaluate_policy(student_model, eval_env, n_eval_episodes=30, return_episode_rewards=True)
             rewards = np.array(rewards)
             ep_lengths = np.array(ep_lengths)
-            logging.info(f"{i} Games Played: {games_played} Running average: {0 if len(scores) == 0 else sum(scores)/len(scores)} Mean reward: {rewards.mean()} +/- {rewards.std()} (over 10 episodes)")
+            logging.info(f"{i} Games Played: {games_played} Running average: {0 if len(scores) == 0 else sum(scores)/len(scores)} Mean reward: {rewards.mean()} +/- {rewards.std()} (over 30 episodes)")
             npz_timesteps.append(50 * i)
             npz_results.append(rewards)
             npz_ep_lengths.append(ep_lengths)
--- run_script.sh
+++ run_script.sh
@@ -1,16 +1,36 @@
 python3 distill.py --teacher_path pong_run_steven/pong_expert.zip --student_model_type BCBL
+mv distill_run/ distill_run_BCBL0/
+python3 distill.py --teacher_path pong_run_steven/pong_expert.zip --student_model_type BCBL
 mv distill_run/ distill_run_BCBL1/
 python3 distill.py --teacher_path pong_run_steven/pong_expert.zip --student_model_type BCBL
 mv distill_run/ distill_run_BCBL2/
+python3 distill.py --teacher_path pong_run_steven/pong_expert.zip --student_model_type SCBL
+mv distill_run/ distill_run_SCBL0/
 python3 distill.py --teacher_path pong_run_steven/pong_expert.zip --student_model_type SCBL
 mv distill_run/ distill_run_SCBL1/
 python3 distill.py --teacher_path pong_run_steven/pong_expert.zip --student_model_type SCBL
 mv distill_run/ distill_run_SCBL2/
 python3 distill.py --teacher_path pong_run_steven/pong_expert.zip --student_model_type BCSL
+mv distill_run/ distill_run_BCSL0/
+python3 distill.py --teacher_path pong_run_steven/pong_expert.zip --student_model_type BCSL
 mv distill_run/ distill_run_BCSL1/
 python3 distill.py --teacher_path pong_run_steven/pong_expert.zip --student_model_type BCSL
 mv distill_run/ distill_run_BCSL2/
 python3 distill.py --teacher_path pong_run_steven/pong_expert.zip --student_model_type SCSL
+mv distill_run/ distill_run_SCSL0/
+python3 distill.py --teacher_path pong_run_steven/pong_expert.zip --student_model_type SCSL
 mv distill_run/ distill_run_SCSL1/
 python3 distill.py --teacher_path pong_run_steven/pong_expert.zip --student_model_type SCSL
 mv distill_run/ distill_run_SCSL2/
+python3 distill.py --teacher_path pong_run_steven/pong_expert.zip --teacher_led
+mv distill_run/ distill_run_TL0/
+python3 distill.py --teacher_path pong_run_steven/pong_expert.zip --teacher_led
+mv distill_run/ distill_run_TL1/
+python3 distill.py --teacher_path pong_run_steven/pong_expert.zip --teacher_led
+mv distill_run/ distill_run_TL2/
+python3 distill.py --teacher_path master.zip
+mv distill_run/ distill_run_master0/
+python3 distill.py --teacher_path master.zip
+mv distill_run/ distill_run_master1/
+python3 distill.py --teacher_path master.zip
+mv distill_run/ distill_run_master2/
